34ded7e3a8cb32c522058be77c563b18516148f8
Bill Spark <jackkerouac1613@gmail.com>
2022-10-30 16:58:19 +0300

    newnewnew

--- js/script.js
+++ js/script.js
@@ -406,6 +406,17 @@ function setBurger(params) {
       btn.setAttribute('aria-expanded', false);
     }
   });
+
+  let burger = document.querySelector('.burger');
+  let menuLinks = menu.querySelectorAll('.header-top__link')
+
+  menuLinks.forEach(function(el){
+    el.addEventListener('click', function(){
+      burger.classList.remove('is-opened');
+      menu.classList.remove('is-opened');
+      document.body.style.overflow = 'auto';
+    })
+  });
 }
 
 setBurger({
@@ -414,49 +425,6 @@ setBurger({
   activeClass: "is-opened",
   hiddenClass: "is-closed"
 });
-
-function setSearch(params) {
-  const openBtn = document.querySelector(`.${params.openBtnClass}`);
-  const search = document.querySelector(`.${params.searchClass}`);
-  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
-
-  search.addEventListener("animationend", function (evt) {
-    if (this._isOpened) {
-      this.classList.remove(params.activeClass);
-      this.classList.remove(params.hiddenClass);
-      this._isOpened = false;
-    } else {
-      this._isOpened = true;
-    }
-  });
-
-  search.addEventListener('click', function(evt) {
-    evt._isSearch = true;
-  });
-
-  openBtn.addEventListener("click", function (evt) {
-    this.disabled = true;
-
-    if (
-      !search.classList.contains(params.activeClass) &&
-      !search.classList.contains(params.hiddenClass)
-    ) {
-      search.classList.add(params.activeClass);
-    }
-  });
-
-  closeBtn.addEventListener('click', function () {
-    openBtn.disabled = false;
-    search.classList.add(params.hiddenClass);
-  });
-
-  document.body.addEventListener('click', function (evt) {
-    if (!evt._isSearch && search._isOpened) {
-      openBtn.disabled = false;
-      search.classList.add(params.hiddenClass);
-    }
-  });
-}
 
 document.addEventListener('DOMContentLoaded', (e) => {
   document.getElementById('open-search-form').addEventListener('click', (e) => {
